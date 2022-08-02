bd8e913cdf8c5063e681558f780e6ce7f88480b0
Feodora Bako <105170258+csakafeda@users.noreply.github.com>
2022-08-02 15:49:55 +0200

    Feature - Add random background for the weather

--- script.js
+++ script.js
@@ -15,11 +15,15 @@ const HUMIDITY_ON_CARD = document.getElementById('humidityOnCard');
 const PRESSURE_ON_CARD = document.getElementById('pressureOnCard');
 const UV_ON_CARD = document.getElementById('uvOnCard');
 const WIND_OR_CARD = document.getElementById('windOnCard');
-
+const SUNNY = "1000";
+const CLOUD = "1003,1006,1009";
+const RAIN = "1063,1087,1150,1153,1168,1171,1183,1186,1189,1192,1195,1198,1201,1243,1246,1273,1276";
+const FOG = "1030,1135,1147";
+const WINTER = "1066,1069,1072,1114,1117,1204,1207,1210,1213,1216,1219,1222,1225,1237,1240,1249,1252,1255,1258,1261,1264,1279,1282";
 
 //CHANGING VARIABLES
 
-let LOCATION_NAME = 'Barcelona';
+let LOCATION_NAME = 'Budapest';
 let ICON = "";
 let CONDITION_TEXT = "";
 let MAX_TEMP = "";
@@ -34,15 +38,16 @@ let UV_LEVEL;
 let WIND;
 let CITY_NAMES;
 let BGRND_IMG;
-
+let LOCATION_CODE;
+let IMG_SEARCHER;
 
 // GET data from weather api
+
 getData();
-bgrndImgChange();
 fillCardWithData();
 
-function getData() {
-    fetch(`http://api.weatherapi.com/v1/current.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
+async function getData() {
+    await fetch(`http://api.weatherapi.com/v1/current.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
         .then(response => response.json())
         .then(data => {
             ICON = data.current.condition.icon;
@@ -53,39 +58,33 @@ function getData() {
             PRESSURE = data.current.pressure_mb;
             UV_LEVEL = data.current.uv;
             WIND = data.current.wind_kph;
-            fillCardWithData();
+            LOCATION_CODE = data.current.condition.code.toString();
+            console.log(LOCATION_CODE)
         })
-    fetch(`http://api.weatherapi.com/v1/astronomy.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
+    await fetch(`http://api.weatherapi.com/v1/astronomy.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
         .then(response => response.json())
         .then(data => {
             SUNRISE = data.astronomy.astro.sunrise;
             SUNSET = data.astronomy.astro.sunset;
         })
-    fetch(`http://api.weatherapi.com/v1/forecast.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
+
+    await fetch(`http://api.weatherapi.com/v1/forecast.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
         .then(response => response.json())
         .then(data => {
             MAX_TEMP = data.forecast.forecastday[0].day.maxtemp_c;
             MIN_TEMP = data.forecast.forecastday[0].day.mintemp_c;
-            fillCardWithData();
+
         })
-
-    // fetch(`http://api.weatherapi.com/v1/search.json?q=${LOCATION_NAME}&key=${WEATHER_API_KEY}`)
-    //     .then(response => response.json())
-    //     .then(data => {
-
-    //     })
+    fillCardWithData();
+    imageSelector();
+    bgrndImgChange();
 }
 
 // Create a simple input field with an autocomplete feature which in after 3 characters a list of cities appear
 
-
-
 //document.createElement('input');
 
-
 // Create a card that shows the weather data (temperature, sky conditions, humidity, etc.) of the selected city
-
-
 
 // Searching and selecting a new city should update the card, event listener - change maybe
 function fillCardWithData() {
@@ -95,35 +94,40 @@ function fillCardWithData() {
     CITY_ON_CARD.innerHTML = LOCATION_NAME;
     DEGREE_ON_CARD.innerHTML = `${ACTUAL_TEMP}` + `<sup>&deg;</sup>`;
     WEATHER_ON_CARD.innerHTML = CONDITION_TEXT;
-    FEELS_LIKE_ON_CARD.innerHTML =`FeelReal:` +FEELSLIKE + `<sup>&deg;</sup>`;
-    WIND_OR_CARD.innerHTML = WIND;
-    PRESSURE_ON_CARD.innerHTML = PRESSURE;
+    FEELS_LIKE_ON_CARD.innerHTML = `FeelReal:` + FEELSLIKE + `<sup>&deg;</sup>`;
+    WIND_OR_CARD.innerHTML = WIND + " km/h";
+    PRESSURE_ON_CARD.innerHTML = PRESSURE + " ";
     UV_ON_CARD.innerHTML = UV_LEVEL;
-    HUMIDITY_ON_CARD.innerHTML = HUMIDITY;
+    HUMIDITY_ON_CARD.innerHTML = HUMIDITY + ` <sup></sup>`;
 }
-
 
 // OPTIONAL TASKS
 
 // Create a button to put a city into favourites, which shows up when the input field is selected and empty
 
-
-
 // When weather panel loading there is a spinning animation (https://codepen.io/wang0nya/pen/bzwQPr)
 
-
-
 // Assign background to chosen city, create object which store city name and image url, use Pexels API to get image
+function imageSelector() {
+    if (SUNNY.includes(LOCATION_CODE)) IMG_SEARCHER = "sun";
+    else if (CLOUD.includes(LOCATION_CODE)) IMG_SEARCHER = "cloud";
+    else if (RAIN.includes(LOCATION_CODE)) IMG_SEARCHER = "rain";
+    else if (FOG.includes(LOCATION_CODE)) IMG_SEARCHER = "fog";
+    else if (WINTER.includes(LOCATION_CODE)) IMG_SEARCHER = "snow";
+    else IMG_SEARCHER = (LOCATION_NAME);
+    console.log(IMG_SEARCHER);
+}
 
-function bgrndImgChange() {
-    fetch(`https://api.pexels.com/v1/search?query=${LOCATION_NAME}`, {
-        headers : {
-            Authorization : PEXELS_API_KEY
+async function bgrndImgChange() {
+    await fetch(`https://api.pexels.com/v1/search?query=${LOCATION_NAME}%20${IMG_SEARCHER}`, {
+        headers: {
+            Authorization: PEXELS_API_KEY
         }
     })
         .then(response => response.json())
         .then(data => {
-            BGRND_IMG = data.photos[Math.floor(Math.random() * (data.photos.length))].src.landscape;
+            //console.log(data)          /// ha lefagyna az oldal, consoleban a "Limit reach ---> várni egy órát, hogy újra lehessen kérni képet"
+            BGRND_IMG = data.photos[Math.floor(Math.random() * 4)].src.landscape;
             BODY.style.backgroundImage = `url("${BGRND_IMG}")`;
         })
 }
